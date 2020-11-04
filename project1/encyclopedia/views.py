519d84d53a30d2eda67005b632662a48ef34b0bc
x.zhang <griffcho16@gmail.com>
2020-11-04 21:25:50 +0900

    added link and its pages

--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +9,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    mrkdown = Markdown()
+    entry = mrkdown.convert(util.get_entry(title))
+
+    if entry:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": entry,
+            "title": title
+        })
+
+    return render(request, "encyclopedia/error.html")
