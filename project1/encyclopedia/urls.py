b393abe84c4b4d8a2a820a87c866e9974972769c
x.zhang <griffcho16@gmail.com>
2020-11-08 15:18:00 +0900

    added edit and create function

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/", views.entry, name="entry"),
     path("random_page/", views.random_page, name="random_page"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create"),
+    path("edit/<str:title>/", views.edit, name="edit"),
 ]
--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,9 +1,17 @@
 import random
+from django import forms
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from markdown2 import Markdown
 from . import util
 
+# Forms
+class CreatePageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(widget=forms.Textarea)
+
+class EditPageForm(forms.Form):
+    content = forms.CharField(widget=forms.Textarea)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -46,3 +54,46 @@ def search(request):
                     "entries": [entry for entry in entries
                                     if query.lower() in entry.lower()]}  
                 )
+
+def create(request):
+    if request.method == "POST":
+        form = CreatePageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+
+            if title.lower() in [entry.lower() for entry in util.list_entries()]:
+                return render(request, "encyclopedia/create.html", {
+                    "form": form,
+                    "error": "This page already exists"
+                })
+
+            util.save_entry(title, content)
+            return redirect(reverse("entry", kwargs={"title": title}))
+
+        else:
+            return render(request, "encyclopedia/create.html", {
+                "form": form
+            })
+
+    return render(request, "encyclopedia/create.html", {
+        "form": CreatePageForm
+    })
+
+def edit(request, title):
+    if request.method == "POST":
+        form = EditPageForm(request.POST)
+        if form.is_valid():
+            content = form.cleaned_data["content"]
+            util.save_entry(title, content)
+            return redirect(reverse("entry", kwargs={"title": title}))
+
+    form = EditPageForm({
+        "content": util.get_entry(title)
+    })
+
+    return render(request, "encyclopedia/edit.html", {
+        "form": form,
+        "title": title
+    })
+
